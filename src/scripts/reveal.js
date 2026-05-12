a1d1ae6383d3450dd452506c68c715e73332c0c0
Dheymerson Nascimento <138606159+Jore-X@users.noreply.github.com>
2026-05-12 20:09:21 -0300

    Add files via upload

---
+++ src/scripts/reveal.js
@@ -0,0 +1,39 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal("#cta .title", {
+  origin: "left",
+  duration: 2000,
+  distance: "20%",
+});
+sr.reveal("#cta .description", {
+  origin: "left",
+  duration: 2000,
+  distance: "20%",
+  delay: 100,
+});
+sr.reveal("#cta_buttons", {
+  origin: "left",
+  duration: 2000,
+  distance: "20%",
+  delay: 150,
+});
+sr.reveal("#cta .social-media-buttons", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "40%",
+});
+sr.reveal(".pizza", {
+  origin: "left",
+  duration: 2000,
+  distance: "20%",
+});
+sr.reveal("#testimonials_deliver", {
+  origin: "left",
+  duration: 1000,
+  distance: "20%",
+});
+sr.reveal(".feedback", {
+  origin: "right",
+  duration: 2000,
+  distance: "20%",
+});
